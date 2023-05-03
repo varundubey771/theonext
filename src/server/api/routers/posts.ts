fb4f18baed1715a4433227a58922773e19bf104b
varundubey771 <varundubey771@gmail.com>
2023-05-03 23:36:31 +0530

    kaafi kuch

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,46 @@
+import { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCClientError } from "@trpc/client";
+import { TRPCError } from "@trpc/server";
+import { use } from "react";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User)=>{
+  return {id:user,
+  username:user.username,profileImageUrl:user.profileImageUrl}
+}
+
 export const postsRouter = createTRPCRouter({
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async({ ctx }) => {
+
+    const posts= await ctx.prisma.post.findMany({
+      take:100,
+    });
+
+    const users= (await clerkClient.users.getUserList({
+      userId:  posts.map((post)=>post.authorId),
+      limit:100
+    })).map(filterUserForClient)
+
+    console.log(users[0])
+    console.log(posts)
+
+    return posts.map((post)=>
+      {
+        const author= users.find((user)=>String(user.id.id)===post.authorId)
+        if(!author) throw new TRPCError({
+          code:"INTERNAL_SERVER_ERROR",
+          message:"author for post not found"
+        })
+
+        return {
+        post,
+        author:(users.find((user)=>String(user.id.id)===post.authorId))
+      }
+    }
+    )
   }),
 });
